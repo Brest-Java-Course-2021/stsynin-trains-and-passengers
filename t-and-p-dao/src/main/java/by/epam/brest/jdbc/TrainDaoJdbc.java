77adc3b0cdbf422a2e7554bb846379a93508f02f
SergeyTsynin <Sergey.Tsynin@gmail.com>
2021-02-21 18:54:03 +0300

    add dao test-db.xml

---
+++ t-and-p-dao/src/main/java/by/epam/brest/jdbc/TrainDaoJdbc.java
@@ -0,0 +1,14 @@
+package by.epam.brest.jdbc;
+
+import by.epam.brest.TrainDao;
+import by.epam.brest.model.Train;
+
+import java.util.List;
+
+public class TrainDaoJdbc implements TrainDao {
+
+    @Override
+    public List<Train> findAll() {
+        return null;
+    }
+}
---
+++ t-and-p-dao/src/test/java/by/epam/brest/jdbc/TrainDaoJdbcTest.java
@@ -0,0 +1,17 @@
+package by.epam.brest.jdbc;
+
+import by.epam.brest.TrainDao;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TrainDaoJdbcTest {
+
+    private TrainDao trainDao;
+
+    @Test
+    public void test_findAll() {
+
+    }
+
+}
