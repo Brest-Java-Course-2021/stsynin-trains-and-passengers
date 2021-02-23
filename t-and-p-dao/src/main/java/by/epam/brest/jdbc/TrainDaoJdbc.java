5b7d639cbe6bb51710acd09f423d3397c9dd06a4
SergeyTsynin <Sergey.Tsynin@gmail.com>
2021-02-23 10:25:38 +0300

    add SpringJUnit4ClassRunner dependency

--- t-and-p-dao/src/main/java/by/epam/brest/jdbc/TrainDaoJdbc.java
+++ t-and-p-dao/src/main/java/by/epam/brest/jdbc/TrainDaoJdbc.java
@@ -2,13 +2,35 @@ package by.epam.brest.jdbc;
 
 import by.epam.brest.TrainDao;
 import by.epam.brest.model.Train;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class TrainDaoJdbc implements TrainDao {
 
+    private static final String SQL_GET_ALL_TRAINS =
+            "SELECT D.TRAIN_ID, D. FROM TRAIN AS D ORDER BY R.TRAIN_NAME";
+
+    NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    public TrainDaoJdbc(DataSource dataSource) {
+        namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+    }
+
     @Override
     public List<Train> findAll() {
-        return null;
+        return namedParameterJdbcTemplate.query(SQL_GET_ALL_TRAINS, new DepartmentRowMapper());
+    }
+
+    private class DepartmentRowMapper implements RowMapper<Train>{
+
+        @Override
+        public Train mapRow(ResultSet resultSet, int i) throws SQLException {
+            return null;
+        }
     }
 }
--- t-and-p-dao/src/test/java/by/epam/brest/jdbc/TrainDaoJdbcTest.java
+++ t-and-p-dao/src/test/java/by/epam/brest/jdbc/TrainDaoJdbcTest.java
@@ -1,17 +1,25 @@
 package by.epam.brest.jdbc;
 
 import by.epam.brest.TrainDao;
+import by.epam.brest.model.Train;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringJUnit4ClassRunner.class)
 public class TrainDaoJdbcTest {
 
     private TrainDao trainDao;
 
     @Test
     public void test_findAll() {
-
+        List<Train> trains = trainDao.findAll();
+        assertNotNull(trains);
+        assertTrue(trains.size() > 0);
     }
 
 }
