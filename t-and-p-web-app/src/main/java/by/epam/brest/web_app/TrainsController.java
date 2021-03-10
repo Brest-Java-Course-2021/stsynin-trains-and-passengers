5fe909d2f0617aa310af183ae10cf696d9406e1a
SergeyTsynin <Sergey.Tsynin@gmail.com>
2021-03-10 14:50:28 +0300

    add add/edit functions into TrainsController

--- t-and-p-web-app/src/main/java/by/epam/brest/web_app/TrainsController.java
+++ t-and-p-web-app/src/main/java/by/epam/brest/web_app/TrainsController.java
@@ -1,14 +1,41 @@
 package by.epam.brest.web_app;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class TrainsController {
 
+    /**
+     * Goto trains list page.
+     *
+     * @return
+     */
+
     @GetMapping(value = "/trains")
-    public String defaultPageRedirect() {
+    public final String trains(Model model) {
         return "trains";
     }
 
+    /**
+     * Goto edit train page.
+     *
+     * @return
+     */
+    @GetMapping(value = "/train/{id}")
+    public final String gotoEditTrainPage(@PathVariable Integer id, Model model) {
+        return "train";
+    }
+
+    /**
+     * Goto add train page.
+     *
+     * @return
+     */
+    @GetMapping(value = "/train/add")
+    public final String gotoAddTrainPage(Model model) {
+        return "train";
+    }
 }
