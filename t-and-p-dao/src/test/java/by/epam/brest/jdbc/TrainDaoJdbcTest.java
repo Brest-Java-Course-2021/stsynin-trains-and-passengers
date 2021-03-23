20b41b70e703fa6fad6dedc1bd56be3a8b158d57
SergeyTsynin <Sergey.Tsynin@gmail.com>
2021-03-23 17:32:47 +0300

    add maven-surefire-plugin

--- t-and-p-dao/src/test/java/by/epam/brest/jdbc/TrainDaoJdbcTest.java
+++ t-and-p-dao/src/test/java/by/epam/brest/jdbc/TrainDaoJdbcTest.java
@@ -14,7 +14,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(locations = {"classpath*:test-db.xml", "classpath*:test-dao.xml"})
+@ContextConfiguration(locations = {"classpath*:test-db.xml", "classpath*:test-dao.xml", "classpath*:test-dao.xml"})
 public class TrainDaoJdbcTest {
 
     @SuppressWarnings("unused")
