202db210c7253d16db19f783a0a646df89affe6c
SergeyTsynin <Sergey.Tsynin@gmail.com>
2021-03-27 14:53:24 +0300

    add service api

---
+++ t-and-p-service-api/src/main/java/by/epam/brest/service/PassengerService.java
@@ -0,0 +1,55 @@
+package by.epam.brest.service;
+
+import by.epam.brest.model.Passenger;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PassengerService {
+
+    /**
+     * Get all passengers from the database.
+     *
+     * @return passengers list.
+     */
+    List<Passenger> findAll();
+
+    /**
+     * Get passenger by Id.
+     *
+     * @param passengerId passenger Id.
+     * @return passenger.
+     */
+    Optional<Passenger> findById(Integer passengerId);
+
+    /**
+     * Save new passenger record.
+     *
+     * @param passenger object.
+     * @return saved passenger Id.
+     */
+    Integer createPassenger(Passenger passenger);
+
+    /**
+     * Update passenger record in the database.
+     *
+     * @param passenger object.
+     * @return number of updated passengers in the database.
+     */
+    Integer updatePassenger(Passenger passenger);
+
+    /**
+     * Delete passenger by Id.
+     *
+     * @param passengerId train Id.
+     * @return number of deleted passengers in the database.
+     */
+    Integer deletePassenger(Integer passengerId);
+
+    /**
+     * Get number of passengers in the database.
+     *
+     * @return number of passengers in the database.
+     */
+    Integer getPassengersCount();
+}
---
+++ t-and-p-service-api/src/main/java/by/epam/brest/service/TrainService.java
@@ -0,0 +1,54 @@
+package by.epam.brest.service;
+
+import by.epam.brest.model.Train;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TrainService {
+    /**
+     * Get all trains from the database.
+     *
+     * @return trains list.
+     */
+    List<Train> findAll();
+
+    /**
+     * Get train by Id.
+     *
+     * @param trainId train Id.
+     * @return train.
+     */
+    Optional<Train> findById(Integer trainId);
+
+    /**
+     * Save new train record.
+     *
+     * @param train object.
+     * @return saved train Id.
+     */
+    Integer createTrain(Train train);
+
+    /**
+     * Update train record in the database.
+     *
+     * @param train object.
+     * @return number of updated trains in the database.
+     */
+    Integer updateTrain(Train train);
+
+    /**
+     * Delete train by Id.
+     *
+     * @param trainId train Id.
+     * @return number of deleted trains in the database.
+     */
+    Integer deleteTrain(Integer trainId);
+
+    /**
+     * Get numbers of trains in the database.
+     *
+     * @return numbers of trains in the database.
+     */
+    Integer getTrainsCount();
+}
