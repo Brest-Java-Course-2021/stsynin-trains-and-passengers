4d57f3009487cb4307286aff6d36f44b170ba397
SergeyTsynin <Sergey.Tsynin@gmail.com>
2021-03-27 17:21:58 +0300

    add service implementation

---
+++ t-and-p-service/src/main/java/by/epam/brest/service/impl/PassengerServiceImpl.java
@@ -0,0 +1,55 @@
+package by.epam.brest.service.impl;
+
+import by.epam.brest.dao.PassengerDao;
+import by.epam.brest.model.Passenger;
+import by.epam.brest.service.PassengerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class PassengerServiceImpl implements PassengerService {
+
+//    Logger logger = LoggerFactory.getLogger(PassengerServiceImpl.class);
+
+    private final PassengerDao passengerDao;
+
+    @Autowired
+    public PassengerServiceImpl(PassengerDao passengerDao) {
+        this.passengerDao = passengerDao;
+    }
+
+    @Override
+    public List<Passenger> findAll() {
+        return passengerDao.findAll();
+    }
+
+    @Override
+    public Optional<Passenger> findById(Integer passengerId) {
+        return passengerDao.findById(passengerId);
+    }
+
+    @Override
+    public Integer createPassenger(Passenger passenger) {
+        return passengerDao.createPassenger(passenger);
+    }
+
+    @Override
+    public Integer updatePassenger(Passenger passenger) {
+        return passengerDao.updatePassenger(passenger);
+    }
+
+    @Override
+    public Integer deletePassenger(Integer passengerId) {
+        return passengerDao.deletePassenger(passengerId);
+    }
+
+    @Override
+    public Integer getPassengersCount() {
+        return passengerDao.getPassengersCount();
+    }
+}
