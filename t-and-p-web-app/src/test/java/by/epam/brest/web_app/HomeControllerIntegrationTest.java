54766fc385deb71d801822a29e9698cd94c0381a
SergeyTsynin <Sergey.Tsynin@gmail.com>
2021-04-21 00:21:58 +0300

    fix HomeControllerIntegrationTest

--- t-and-p-web-app/src/test/java/by/epam/brest/web_app/HomeControllerIntegrationTest.java
+++ t-and-p-web-app/src/test/java/by/epam/brest/web_app/HomeControllerIntegrationTest.java
@@ -2,11 +2,8 @@ package by.epam.brest.web_app;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
@@ -18,9 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 /**
  * @author Sergey Tsynin
  */
-@ExtendWith(SpringExtension.class)
-@WebAppConfiguration
-@ContextConfiguration(locations = {"classpath:app-context-test.xml"})
+@SpringBootTest
 class HomeControllerIntegrationTest {
 
     @Autowired
