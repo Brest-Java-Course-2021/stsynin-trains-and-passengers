2f980eea94c0bba0c8fe8779f6fd488f232fd2d3
SergeyTsynin <Sergey.Tsynin@gmail.com>
2021-05-02 11:05:46 +0300

    fix home controller test

--- t-and-p-web-app/src/test/java/by/epam/brest/web_app/HomeControllerTest.java
+++ t-and-p-web-app/src/test/java/by/epam/brest/web_app/HomeControllerTest.java
@@ -1,32 +1,22 @@
 package by.epam.brest.web_app;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * @author Sergey Tsynin
  */
-@SpringBootTest
-class HomeControllerIntegrationTest {
+@WebMvcTest(controllers = HomeController.class)
+class HomeControllerTest {
 
     @Autowired
-    private WebApplicationContext wac;
-
     private MockMvc mockMvc;
-
-    @BeforeEach
-    public void setup() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
-    }
 
     @Test
     void shouldRedirectTrainsPage() throws Exception {
