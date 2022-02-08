aa1c8c09ad576201b7a842eef0326922e997191b
ay-3-8910 <Sergey.Tsinin@gmail.com>
2022-02-08 10:27:32 +0300

    add Json serializer

--- model/src/main/java/by/epam/brest/model/Train.java
+++ model/src/main/java/by/epam/brest/model/Train.java
@@ -1,5 +1,10 @@
 package by.epam.brest.model;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+
 import java.time.LocalDate;
 import java.util.Objects;
 
@@ -11,6 +16,8 @@ public class Train {
 
     private String trainDestination;
 
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
     private LocalDate trainDepartureDate;
 
     public Train() {
