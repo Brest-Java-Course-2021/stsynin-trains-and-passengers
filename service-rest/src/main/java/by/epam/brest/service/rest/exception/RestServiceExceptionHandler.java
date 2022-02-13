ecc6577a7f8293d5d458620c159b7b6f9d39a535
SergeyTsynin <Sergey.Tsinin@gmail.com>
2022-02-13 21:13:55 +0300

    return not found error message without redirect

--- service-rest/src/main/java/by/epam/brest/service/rest/exception/RestServiceExceptionHandler.java
+++ service-rest/src/main/java/by/epam/brest/service/rest/exception/RestServiceExceptionHandler.java
@@ -5,7 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author Sergey Tsynin
@@ -20,9 +20,19 @@ public class RestServiceExceptionHandler {
     private static final Logger LOGGER = LoggerFactory.getLogger(RestServiceExceptionHandler.class);
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public String handleNotFoundError(ResourceNotFoundException e, RedirectAttributes redirectAttributes) {
-        LOGGER.error(e.getMessage());
-        redirectAttributes.addAttribute("errorMessage", e.getMessage());
-        return "redirect:/error";
+    public ModelAndView handleNotFoundError(ResourceNotFoundException e) {
+        String errorMessage = e.getMessage();
+        LOGGER.error("OUT: handleNotFoundError - [{}]", errorMessage);
+        ModelAndView errorPage = new ModelAndView("error");
+        errorPage.addObject("errorMessage", "We're sorry, but we can't find anything about this.");
+        errorPage.addObject("errorDescription", errorMessage);
+        return errorPage;
     }
+//
+//    @ExceptionHandler(ResourceNotFoundException.class)
+//    public String handleNotFoundError(ResourceNotFoundException e, RedirectAttributes redirectAttributes) {
+//        LOGGER.error(e.getMessage());
+//        redirectAttributes.addAttribute("errorMessage", e.getMessage());
+//        return "redirect:/error";
+//    }
 }
