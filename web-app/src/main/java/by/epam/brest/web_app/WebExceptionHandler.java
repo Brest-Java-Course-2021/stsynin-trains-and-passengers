aa416beae2039945355d53786d27a2abd7becf90
SergeyTsynin <Sergey.Tsinin@gmail.com>
2022-02-14 14:19:00 +0300

    add handleValidationError

--- web-app/src/main/java/by/epam/brest/web_app/WebExceptionHandler.java
+++ web-app/src/main/java/by/epam/brest/web_app/WebExceptionHandler.java
@@ -1,6 +1,7 @@
 package by.epam.brest.web_app;
 
 import by.epam.brest.model.ErrorMessage;
+import by.epam.brest.model.exception.ValidationErrorException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,7 +19,7 @@ import java.io.IOException;
 public class WebExceptionHandler {
 
     public WebExceptionHandler() {
-        LOGGER.info("RestServiceExceptionHandler was created");
+        LOGGER.info("WebExceptionHandler was created");
     }
 
     private final ObjectMapper objectMapper = new ObjectMapper();
@@ -28,22 +29,37 @@ public class WebExceptionHandler {
     @ExceptionHandler(HttpClientErrorException.NotFound.class)
     public ModelAndView handleNotFoundError(HttpClientErrorException.NotFound e) throws IOException {
         String errorMessage = extractErrorMessage(e.getResponseBodyAsString()).getMessage();
-        LOGGER.error("OUT: handleNotFoundError - [{}]", errorMessage);
+        LOGGER.error(" IN: handleNotFoundError - [{}]", errorMessage);
         ModelAndView errorPage = new ModelAndView("error");
         errorPage.addObject("errorMessage",
                 "We're sorry, but we can't find anything about this.");
         errorPage.addObject("errorDescription", errorMessage);
+        LOGGER.error("OUT: handleNotFoundError - [errorPage]");
         return errorPage;
     }
 
     @ExceptionHandler(HttpClientErrorException.Conflict.class)
     public ModelAndView handleResourceLockedError(HttpClientErrorException.Conflict e) throws IOException {
         String errorMessage = extractErrorMessage(e.getResponseBodyAsString()).getMessage();
-        LOGGER.error("OUT: handleResourceLockedError - [{}]", errorMessage);
+        LOGGER.error(" IN: handleResourceLockedError - [{}]", errorMessage);
         ModelAndView errorPage = new ModelAndView("error");
         errorPage.addObject("errorMessage",
-                "We're sorry, but we can't delete loaded train. You should remove passenger(s) first.");
+                "We are sorry, but we cannot remove a loaded train." +
+                        "You must first remove the passenger(s).");
         errorPage.addObject("errorDescription", errorMessage);
+        LOGGER.error("OUT: handleResourceLockedError - [errorPage]");
+        return errorPage;
+    }
+
+    @ExceptionHandler(ValidationErrorException.class)
+    public ModelAndView handleValidationError(ValidationErrorException e) throws IOException {
+        String errorMessage = e.getMessage();
+        LOGGER.error(" IN: handleValidationError - [{}]", errorMessage);
+        ModelAndView errorPage = new ModelAndView("error");
+        errorPage.addObject("errorMessage",
+                "We don't know how it happened, but there was a mistake in the data you entered.");
+        errorPage.addObject("errorDescription", errorMessage);
+        LOGGER.error("OUT: handleValidationError - [errorPage]");
         return errorPage;
     }
 
